e288925eae846d1bd9bcf0c47bf540c9986acc84
unknown <b03418@099w6027.bh.com.ar>
2012-10-10 15:01:40 -0300

    Added structure for assigment2

---
+++ assigment2/getiutput.R
@@ -0,0 +1,14 @@
+# defining a function 
+is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1]) 
+
+if(!is.installed("digest"))
+  install.packages("digest")
+
+if(!is.installed("RCurl"))
+  install.packages("RCUrl")
+
+source("http://spark-public.s3.amazonaws.com/compdata/scripts/submitscript.R")
+
+#Uncomment this when the package works
+#submit()
+
